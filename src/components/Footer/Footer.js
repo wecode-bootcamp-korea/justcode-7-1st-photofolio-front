7950871ff968fa0de1acea736bc4ab9ce2108127
DawonOh <dawonohh@gmail.com>
2022-11-01 12:54:37 +0900

    footer 추가

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,17 @@ import React from 'react';
 import css from './Footer.module.scss';
 
 function Footer() {
-  return <div className={css.container}></div>;
+  return (
+    <footer className={css.footerContainer}>
+      <div className={css.footerAlign}>
+        <div className={css.footerText}>
+          <span>이용약관</span>
+          <span>개인정보 처리방침</span>
+          <span>ⓒ Photofolio.</span>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
