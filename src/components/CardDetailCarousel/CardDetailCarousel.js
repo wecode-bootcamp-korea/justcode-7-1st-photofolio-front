91576659d4469c8213b061e22e27d661b01c35f2
DawonOh <dawonohh@gmail.com>
2022-11-03 16:03:16 +0900

    Add: src/components/CardDetailCarousel 폴더 및 파일 추가 - CardDetailCarousel.js 파일 추가 - CardDetailCarousel.scss 파일 추가

---
+++ src/components/CardDetailCarousel/CardDetailCarousel.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './CardDetailCarousel.scss';
+
+const CardDetailCarousel = () => {
+  return (
+    <Fragment>
+      <div></div>
+    </Fragment>
+  );
+};
+
+export default CardDetailCarousel;
