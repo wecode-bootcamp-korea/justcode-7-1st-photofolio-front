c40f4ef6c5209fb4f64920ae0b5bc51086080f9a
DawonOh <dawonohh@gmail.com>
2022-11-07 22:56:44 +0900

    Add:팔로잉 기능 구현 완료

--- src/components/CardDetailCarousel/CardDetailCarousel.js
+++ src/components/CardDetailCarousel/CardDetailCarousel.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './CardDetailCarousel.scss';
 import Login from '../Login/Login';
 import Join from '../Join/Join';
@@ -7,6 +7,7 @@ const CardDetailCarousel = () => {
   const [info, setInfo] = useState({});
   const [works, setWorks] = useState([]);
   const [followers, setFollowers] = useState([]);
+  const [isFollow, setIsFollow] = useState(0);
 
   //login창 로직 추가 코드
   const [openLoginpage, setOpenLoginPage] = useState(false);
@@ -34,6 +35,7 @@ const CardDetailCarousel = () => {
     }
   }, [token]);
 
+  //페이지 첫 렌더링 시 데이터 불러오기
   useEffect(() => {
     fetch('/data/cardDetailWriterInfo.json')
       .then(res => res.json())
@@ -41,116 +43,136 @@ const CardDetailCarousel = () => {
         setInfo(json.data[0]);
         setWorks(json.data[0].works);
         setFollowers(json.data[0].followers);
+        setIsFollow(json.data[0].isFollow);
       });
   }, []);
 
-  const handleRightClick = () => {
-    setIsClick(true);
-  };
-  const handleLeftClick = () => {
-    setIsClick(false);
-  };
-
   //클릭 여부 확인
-  const isFollowNow = info.isFollow;
-  console.log(info);
-  console.log('info.isFollow : ', info.isFollow);
-  const [isClick, setIsClick] = useState(isFollowNow);
+  const [isClick, setIsClick] = useState(isFollow);
+  const handleToggle = () => {
+    setIsClick(!isClick);
+  };
 
-  const changeFollowBtn = () => {
-    console.log('changeFollowBtn 실행!');
-    // 팔로잉 되어있는 상태
-    if (isClick === true) {
-      console.log('if문 안 isClick : ', isClick);
-      //isClick이 true면 하얀버튼 상태
-      setIsClick(false);
-      //fetch(백엔드에게 언팔로우 했다는 데이터 보내기) - DELETE / 작가id, 토큰
-      return;
-    } else {
-      //isClick이 false이면 초록버튼 상태
-      console.log('else문 안의 isClick : ', isClick);
-      setIsClick(true);
-      //fetch(백엔드에게 팔로우 했다는 데이터 보내기) - POST / 작가id, 토큰
-      return;
+  //팔로우,언팔로우 함수
+  const sendResult = e => {
+    if (e.target.className === 'followBtn') {
+      //POST 작가id, 토큰
+      fetch('http://localhost:8000/works/following', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          token: localStorage.getItem('token'),
+        },
+        body: JSON.stringify({
+          following_id: info.id,
+        }),
+      })
+        .then(res => res.json())
+        .then(json => {});
+    } else if (e.target.className === 'followingBtn') {
+      //DELETE 작가id, 토큰
+      fetch('http://localhost:8000/works/following-cancel', {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          token: localStorage.getItem('token'),
+        },
+        body: JSON.stringify({
+          following_id: info.id,
+        }),
+      })
+        .then(res => res.json())
+        .then(json => {});
     }
   };
-  console.log('함수 밖에서 isClick================ : ', isClick);
+  console.log(info.id);
 
   return (
-    <div className="cardDetailInfoCarousel">
-      {/* login창 로직 추가 코드 */}
-      {openLoginpage && (
-        <Login
-          closeLoginpage={closeLoginpage}
-          setJoinPage={setJoinPage}
-          setOpenLoginPage={setOpenLoginPage}
-        />
-      )}
-      {openJoinPage && <Join setJoinPage={setJoinPage} />}
-      {/* login창 로직 추가 코드 종료*/}
-      <div className="writerInfo">
-        <div className="writerInfoLeft">
-          <div className="writerInfoImg">
-            <img src={info.profile_img} alt="작가프로필이미지" />
-          </div>
-          <div className="writerInfoText">
-            <h3>{info.kor_name}</h3>
-            <h3>{info.eng_name}</h3>
-            <div className="writerInfoTextDiv">
-              <span className="writersFollow">
-                팔로워<span>{info.followers_count}</span>
-              </span>
-              <span className="writersFollow">
-                팔로잉<span>{info.followings_count}</span>
-              </span>
+    <>
+      <div className="wideBorder"></div>
+      <div className="cardDetailInfoCarousel">
+        {/* login창 로직 추가 코드 */}
+        {openLoginpage && (
+          <Login
+            closeLoginpage={closeLoginpage}
+            setJoinPage={setJoinPage}
+            setOpenLoginPage={setOpenLoginPage}
+          />
+        )}
+        {openJoinPage && <Join setJoinPage={setJoinPage} />}
+        {/* login창 로직 추가 코드 종료*/}
+        <div className="writerInfo">
+          <div className="writerInfoLeft">
+            <div className="writerInfoImg">
+              <img src={info.profile_img} alt="작가프로필이미지" />
+            </div>
+            <div className="writerInfoText">
+              <h3>{info.kor_name}</h3>
+              <h3>{info.eng_name}</h3>
+              <div className="writerInfoTextDiv">
+                <span className="writersFollow">
+                  팔로워<span>{info.followers_count}</span>
+                </span>
+                <span className="writersFollow">
+                  팔로잉<span>{info.followings_count}</span>
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="followBtn" onClick={changeFollowBtn}>
-          {/* 로그인 한 상태이면서 팔로우한 상태에서만 팔로우 버튼 */}
-          {isLogin ? (
-            info.isFollow ? (
-              <div className="followingBtn">팔로잉</div>
+          <div className="followBtns" onClick={handleToggle}>
+            {/* 
+            로그인 한 상태이면서 팔로우한 상태에서만 팔로잉 버튼 
+            1 : 팔로잉 되어있는 상태 (하얀 버튼)
+            0 : 팔로잉 되어있지 않은 상태 (초록 버튼)
+            */}
+            {isLogin ? (
+              isFollow === 1 ? (
+                <div
+                  className={isClick ? 'followBtn' : 'followingBtn'}
+                  onClick={sendResult}
+                >
+                  {isClick ? '팔로우' : '팔로잉'}
+                </div>
+              ) : (
+                <div
+                  className={isClick ? 'followingBtn' : 'followBtn'}
+                  onClick={sendResult}
+                >
+                  {isClick ? '팔로잉' : '팔로우'}
+                </div>
+              )
             ) : (
-              <div className="followBtn">팔로우</div>
-            )
-          ) : (
-            <div className="followBtn" onClick={clickLoginBtn}>
-              팔로우
-            </div>
-          )}
-        </div>
-      </div>
-      {/* Carousel */}
-      <div className="otherWorksCarousel">
-        <h3>이 크리에이터의 다른 작품</h3>
-        <span>{info.works_count}</span>
-        <div className="otherWorkBtnContainer">
-          <div
-            className="otherWorksBtn otherWorksPrev"
-            onClick={handleLeftClick}
-          />
-          <div className="otherWorksCarouselContainer">
-            <ul>
-              {works.map(work => {
-                return (
-                  <li className="otherWorksItem" key={work.id}>
-                    <div className="otherWorksImg">
-                      <img src={work.image} alt={work.title} />
-                    </div>
-                    <div className="otherWorksTitle">{work.title}</div>
-                  </li>
-                );
-              })}
-            </ul>
+              <div className="followBtn" onClick={clickLoginBtn}>
+                팔로우
+              </div>
+            )}
           </div>
-          <div
-            className="otherWorksBtn otherWorksNext"
-            onClick={handleRightClick}
-          />
+        </div>
+        {/* Carousel */}
+        <div className="otherWorksCarousel">
+          <h3>이 크리에이터의 다른 작품</h3>
+          <span>{info.works_count}</span>
+          <div className="otherWorkBtnContainer">
+            <div className="otherWorksBtn otherWorksPrev" />
+            <div className="otherWorksCarouselContainer">
+              <ul className="goToLeft">
+                {works.map(work => {
+                  return (
+                    <li className="otherWorksItem" key={work.id}>
+                      <div className="otherWorksImg">
+                        <img src={work.image} alt={work.title} />
+                      </div>
+                      <div className="otherWorksTitle">{work.title}</div>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className="otherWorksBtn otherWorksNext" />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
