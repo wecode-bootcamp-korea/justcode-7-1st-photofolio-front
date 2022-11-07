2adbbda8888fdd04ab3e6cde11450d22641241c3
hazel-han99 <tbxl67@naver.com>
2022-11-08 02:59:05 +0900

    댓글기능 구현 완료, 로그인에 따라 댓글창 조건부 렌더링 완료

--- src/components/CardDetailContent/CardDetailContent.js
+++ src/components/CardDetailContent/CardDetailContent.js
@@ -1,16 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './CardDetailContent.scss';
 import Tag from './Tag/Tag';
 import Reply from './Reply/Reply';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const CardDetailContents = () => {
   const [cardDetailContents, setcardDetailContents] = useState([]);
   const [tags, setTags] = useState([]); //태크
   const [sympathys, setSympathys] = useState([]); //공감
   const [isClick, setIsClick] = useState(false); // 댓글꺽쇠
-  let [reply, setReply] = useState(''); //댓글
   let [replyArray, setReplyArray] = useState([]); //댓글배열
+  let [id, setId] = useState(1); //댓글의 id
+  const value = useRef();
+  const navigate = useNavigate();
 
   //데이터 fetch
   useEffect(() => {
@@ -24,26 +26,62 @@ const CardDetailContents = () => {
       });
   }, []);
 
-  //아이콘 변경 함수
-  const iconChange = () => {
-    if (isClick === true) {
-      setIsClick(false);
-    } else {
-      setIsClick(true);
-    }
-  };
+  //카드 상세페이지 정보 fetch
+  // useEffect(() => {
+  //   fetch('http://localhost:8000/user/accountInfo', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   })
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       setcardDetailContents(res.data[0]);
+  //       setTags(res.data[0].tag);
+  //       setSympathys(res.data[0].Sympathy[0]);
+  //       setReplyArray(res.data[0].Comment);
+  //     });
+  // }, []);
 
   //댓글 추가 함수
-  let addComment = e => {
-    const copyReplyArray = [...replyArray];
-    copyReplyArray.push(reply);
-    setReplyArray(copyReplyArray);
-    setReply('');
+  const addReply = () => {
+    //현재날짜
+    const date = new Date();
+    //아이디 증가(겹치지 않게)
+    setId(id + 1);
+    const newReply = {
+      writer: '혜선', //localStorage.getItem('kor_name'),
+      id: id,
+      content: value.current.value,
+      regidate: date.toLocaleDateString('ko-kr'), //현재 날짜에서 연도 구하는 함수
+    };
+    setReplyArray([...replyArray, newReply]);
+    value = '';
   };
-  console.log(replyArray);
+  //새로운 배열에 reverse 배열 적용
+  const reverseReplyArray = replyArray.reverse();
+
+  //새로운 댓글 저장 fetch
+  // useEffect(() => {
+  //   fetch('http://localhost:8000/user/accountInfo', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       newReply: replyArray.newReply(),
+  //     },
+  //   })
+  //     .then(res => res.json())
+  //     .then(res => setReplyArray(res.data));
+  // }, [replyArray]);
+
+  //로그인하지 않았을 때 댓글창 누르면, 메인으로 이동
+  const movePage = () => {
+    alert('로그인한 다음 이용해 주세요.');
+    navigate('/works');
+  };
 
   return (
-    <div>
+    <>
       <div className="detail-out-wrapper">
         <div className="detail-header-wrapper">
           <div className="detail-title-wrapper">
@@ -102,68 +140,55 @@ const CardDetailContents = () => {
             <span className="detail-reply-count">
               {cardDetailContents.replyArray_count}
             </span>
-            <button className="detail-reply-add-btn" onClick={iconChange}>
-              {isClick === true ? (
-                <img
-                  className="detail-reply-add-btn-down"
-                  src="https://cdn-icons-png.flaticon.com/512/130/130906.png"
-                  alt=""
-                />
-              ) : (
-                <img
-                  className="detail-reply-add-btn-up"
-                  src="https://cdn-icons-png.flaticon.com/512/2985/2985150.png"
-                  alt=""
-                />
-              )}
-            </button>
           </div>
         </div>
-        <div className="detail-reply-input-out-wrapper">
-          <div className="detail-reply-input-wrapper">
-            <div className="detail-reply-input-inner-wrapper">
-              <div className="detail-reply-title">혜선</div>
-              <div className="detail-reply-text-area-wrapper">
-                {localStorage.getItem('token') ? (
-                  <textarea
-                    type="text"
-                    placeholder="주제와 무관한 댓글, 악플은 삭제될 수 있습니다."
-                    onClick={e => {
-                      setReply(e.target.value);
-                    }}
-                  />
-                ) : (
-                  <textarea
-                    type="text"
-                    placeholder="댓글을 작성하려면 로그인 해주세요."
-                    onClick={e => {
-                      setReply(e.target.value);
-                    }}
-                  />
-                )}
+        <details>
+          <summary></summary>
+          <div className="detail-reply-input-out-wrapper">
+            <div className="detail-reply-input-wrapper">
+              <div className="detail-reply-input-inner-wrapper">
+                <div className="detail-reply-title">
+                  혜선
+                  {/* {localStorage.getItem('kor_name')} */}
+                </div>
+                <div className="detail-reply-text-area-wrapper">
+                  {localStorage.getItem('token') ? (
+                    <textarea
+                      type="text"
+                      placeholder="주제와 무관한 댓글, 악플은 삭제될 수 있습니다."
+                      ref={value}
+                    />
+                  ) : (
+                    <textarea
+                      type="text"
+                      placeholder="댓글을 작성하려면 로그인 해주세요."
+                      onClick={movePage}
+                    />
+                  )}
+                </div>
+                <div className="detail-reply-text-limit">최대 1000자</div>
               </div>
-              <div className="detail-reply-text-limit">0/1000</div>
+              <div className="detail-reply-apload-btn">
+                <button onClick={addReply}>등록</button>
+              </div>
             </div>
-            <div className="detail-reply-apload-btn">
-              <button onClick={addComment}>등록</button>
+            <div className="detail-reply-list">
+              {/* 댓글 컴포넌트 */}
+              {reverseReplyArray.map(comment => {
+                return (
+                  <Reply
+                    key={comment.id}
+                    writer={comment.writer}
+                    content={comment.content}
+                    regidate={comment.regidate}
+                  />
+                );
+              })}
             </div>
           </div>
-          <div className="detail-reply-list">
-            {/* 댓글 컴포넌트 */}
-            {replyArray.map(comment => {
-              return (
-                <Reply
-                  key={comment.id}
-                  writer={comment.writer}
-                  content={comment.content}
-                  regidate={comment.regidate}
-                />
-              );
-            })}
-          </div>
-        </div>
+        </details>
       </div>
-    </div>
+    </>
   );
 };
 
