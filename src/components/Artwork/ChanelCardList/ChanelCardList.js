504fec8ea6c6c503d99e308da7fc7a43fbd814e4
RBGonion <yopi27@naver.com>
2022-11-07 11:06:56 +0900

    채널용 카드리스트 생성

---
+++ src/components/Artwork/ChanelCardList/ChanelCardList.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import Card from './ChanelCard';
+import './chanelCardList.scss';
+
+function CardList({ filter }) {
+  const [data, setData] = useState([]);
+
+  fetch('/data/cardListData.json')
+    .then(res => res.json())
+    .then(data => setData(data.data));
+
+  return (
+    <div className="cardList">
+      {data.map((elem, idx) => {
+        return <Card key={idx} />;
+      })}
+    </div>
+  );
+}
+
+export default CardList;
