854131ff2e016e579d9fe8ac3b3503fc959e7346
DawonOh <dawonohh@gmail.com>
2022-11-08 16:05:20 +0900

    Add: 캐러셀 기능 추가

--- src/components/CardDetailCarousel/CardDetailCarousel.js
+++ src/components/CardDetailCarousel/CardDetailCarousel.js
@@ -85,8 +85,51 @@ const CardDetailCarousel = () => {
         .then(json => {});
     }
   };
-  console.log(info.id);
 
+  //캐러셀 내부 ul 위치 파악
+  //0이면 맨 처음(index같은 느낌)
+  const [carouselIndex, setCarouselIndex] = useState(0);
+
+  //carouselUl의 값에 따라 translateX값 추가
+  const [listLocation, setListLocation] = useState(0);
+
+  //onclick으로 걸 함수 : carouselUl 값 조절
+  //클릭 시 1추가, px -추가
+  //최대 페이지 : 작품 갯수로 판단
+  const controlCarouselUlNext = () => {
+    if (works.length % 5 !== 0) {
+      let maxPage = Math.floor(works.length / 5);
+      console.log('maxPage : ', maxPage);
+
+      if (carouselIndex === maxPage) {
+        return;
+      } else {
+        setCarouselIndex(carouselIndex + 1);
+        setListLocation(listLocation - 910);
+      }
+      return;
+    } else if (works.length % 5 === 0) {
+      setCarouselIndex(Math.floor(works.length / 5));
+      setListLocation(listLocation - 910);
+      return;
+    }
+  };
+
+  //클릭 시 1감소 (0보다 작아지지 않게 처리)
+  //px +추가
+  const controlCarouselUlPrev = () => {
+    if (carouselIndex === 0) {
+      setListLocation(0);
+      return;
+    } else {
+      setCarouselIndex(carouselIndex - 1);
+      setListLocation(listLocation + 910);
+      return;
+    }
+  };
+
+  let trans = 'translateX(' + listLocation + 'px)';
+  let transCarousel = { transform: trans };
   return (
     <>
       <div className="wideBorder"></div>
@@ -153,22 +196,30 @@ const CardDetailCarousel = () => {
           <h3>이 크리에이터의 다른 작품</h3>
           <span>{info.works_count}</span>
           <div className="otherWorkBtnContainer">
-            <div className="otherWorksBtn otherWorksPrev" />
+            <div
+              className="otherWorksBtn otherWorksPrev"
+              onClick={controlCarouselUlPrev}
+            />
             <div className="otherWorksCarouselContainer">
-              <ul className="goToLeft">
-                {works.map(work => {
-                  return (
-                    <li className="otherWorksItem" key={work.id}>
-                      <div className="otherWorksImg">
-                        <img src={work.image} alt={work.title} />
-                      </div>
-                      <div className="otherWorksTitle">{work.title}</div>
-                    </li>
-                  );
-                })}
+              <ul>
+                <div className="liList" style={transCarousel}>
+                  {works.map(work => {
+                    return (
+                      <li className="otherWorksItem" key={work.id}>
+                        <div className="otherWorksImg">
+                          <img src={work.image} alt={work.title} />
+                        </div>
+                        <div className="otherWorksTitle">{work.title}</div>
+                      </li>
+                    );
+                  })}
+                </div>
               </ul>
             </div>
-            <div className="otherWorksBtn otherWorksNext" />
+            <div
+              className="otherWorksBtn otherWorksNext"
+              onClick={controlCarouselUlNext}
+            />
           </div>
         </div>
       </div>
