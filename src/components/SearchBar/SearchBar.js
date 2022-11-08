fc1ccd71adacdd0a011cd13f043d380a95e86f0f
DawonOh <dawonohh@gmail.com>
2022-11-08 22:43:02 +0900

    Add: 검색창 레이아웃 추가

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,12 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
 import './SearchBar.scss';
 
 const SearchBar = () => {
+  const [content, setContent] = useState();
+  const nowContent = useRef();
+  const handleKeyUp = e => {
+    if (e.key === 'Enter') {
+      console.log('검색!');
+    } else {
+      setContent(nowContent.current.innerText);
+      console.log('content : ', content);
+    }
+  };
   return (
     <Fragment>
       <div className="searchKeyWordBar">
         <div className="searchKeyWord">
-          <input type="text" placeholder="검색어를 입력해주세요." />
+          <div className="searchLogoImg" />
+          <div
+            className="inputDiv"
+            contentEditable="true"
+            placeholder="검색어를 입력해주세요."
+            ref={nowContent}
+            onKeyUp={handleKeyUp}
+          ></div>
         </div>
       </div>
     </Fragment>
