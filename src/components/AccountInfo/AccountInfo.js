1088a17e89bcc5564d46ab041694729166a2e861
hazel-han99 <tbxl67@naver.com>
2022-11-10 18:04:14 +0900

    파일 충돌 방지

--- src/components/AccountInfo/AccountInfo.js
+++ src/components/AccountInfo/AccountInfo.js
@@ -1,229 +1,201 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './AccountInfo.scss';
-import DeleteModal from './DeleteModal/DeleteModal';
+import React, { useEffect, useRef, useState } from 'react';
+import './CardDetailContent.scss';
+import Tag from './Tag/Tag';
+import Reply from './Reply/Reply';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
-const AccountInfo = () => {
-  const navigate = useNavigate();
-  const [modalOpen, setModalOpen] = useState(false);
-  const [accountInfo, setAccountInfo] = useState({
-    login_id: '',
-    kor_name: '',
-    eng_name: '',
-    email: '',
-    nickname: '',
-    profile_image: '',
-  });
+const CardDetailContents = () => {
+  const [cardDetailContents, setcardDetailContents] = useState([]);
+  const [tags, setTags] = useState([]); //태그
+  const [feedImg, setFeedImg] = useState([]);
+  let [sympathys, setSympathys] = useState([]); //공감배열
+  const [userId, setUserId] = useState([]); //user_id
 
-  //accountInfo에 변화가 생겼을 때(수정)
-  const onChange = e => {
-    const { name, value } = e.target;
-    setAccountInfo({
-      ...accountInfo,
-      [name]: value,
-    });
-  };
+  const { id } = useParams();
 
-  //계정정보 fetch
+  //카드 상세페이지 정보 fetch
   useEffect(() => {
-    fetch('http://localhost:8000/user/accountInfo', {
-      method: 'POST',
+    fetch('http://localhost:8000/works/' + id, {
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         token: localStorage.getItem('token'),
       },
     })
       .then(res => res.json())
-      .then(result => setAccountInfo(result.data));
-  }, []);
+      .then(res => {
+        setcardDetailContents(res.feedWithTags[0]);
+        setUserId(res.feedWithTags[0].user_id);
+        setTags(res.feedWithTags[0].tagInfo);
+        setSympathys(res.sympathySortCount[0]);
+        setReplyArray(res.feedCommentInfo);
+        setFeedImg(res.feedImgArr[0].fileInfo[0]);
+      });
+  }, [id]);
 
-  // 삭제 모달창 노출
-  const showModal = e => {
-    e.preventDefault();
-    setModalOpen(true);
-  };
+  const reply = useRef(); //현재 댓글의 value
+  const [replyArray, setReplyArray] = useState([]); //댓글배열
 
-  //이미지 수정
-  // function ImgChange(e) {
-  //   console.log(e.target.files);
-  //   setAccountInfo([...e.target.files]);
-  //   // setUploadCheck(false);
-  // }
-
-  //수정된 계정정보 서버로 저장
-  const saveAccountInfo = e => {
-    e.preventDefault();
-    fetch('http://localhost:8000/user/accountInfo', {
-      method: 'PATCH',
+  //새로운 댓글 저장 fetch
+  const saveReply = () => {
+    fetch('http://localhost:8000/comments', {
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         token: localStorage.getItem('token'),
       },
-      body: JSON.stringify(accountInfo),
-    }).then(res => res.json());
+      body: JSON.stringify({ id: id, comment: reply.current.value }),
+    })
+      .then(res => res.json())
+      .then(res => setReplyArray(res.data));
   };
+
+  const navigate = useNavigate();
+
+  //로그인하지 않았을 때 댓글창 누르면, 메인으로 이동
+  const movePage = () => {
+    alert('로그인한 다음 이용해 주세요.');
+    navigate('/works');
+  };
+
+  //현재 좋아요 갯수 상태
+  // const [likeCnt, setLikeCnt] = useState('');
+
+  // //좋아요 누르면 실행되는 로직
+  // const clickLike = () => {
+  //   fetch('http://localhost:8000/works/sympathy', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       token: localStorage.getItem('token'),
+  //     },
+  //     body: {
+  //       posting_id: id,
+  //       sympathy_id: 1,
+  //     },
+  //   })
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       setLikeCnt(res.data);
+  //     });
+  // };
 
   return (
     <div>
-      <div className="account-out-wrapper">
-        <div className="account-header-wrapper">
-          <div className="account-header">계정정보</div>
+      <div className="detail-out-wrapper">
+        <div className="detail-header-wrapper">
+          <div className="detail-title-wrapper">
+            <h3 className="detail-title">{cardDetailContents.title}</h3>
+          </div>
+          <span className="detail-writer-by">by</span>
+          <button
+            className="detail-writer-nickname"
+            onClick={() => navigate(`/channel/${userId}`)}
+          >
+            {cardDetailContents.kor_name}
+          </button>
+          <span className="detail-date">{cardDetailContents.created_at}</span>
+          <span className="detail-inquiry-count">
+            조회수{cardDetailContents.view_count}
+          </span>
         </div>
-        <div className="account-info-wrapper">
-          <div className="account-info-form-wrapper">
-            <form className="account-info-form">
-              <table>
-                <tbody>
-                  <tr>
-                    <td>
-                      <div className="account-info-title special">로그인ID</div>
-                      <div name="login_id" className="account-info-id">
-                        {accountInfo.login_id}
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-                <tbody>
-                  <tr>
-                    <td>
-                      <div className="account-info-not-null">*</div>
-                      <div className="account-info-title">이름</div>
-                      <input
-                        name="kor_name"
-                        type="text"
-                        onChange={onChange}
-                        defaultValue={accountInfo?.kor_name}
-                        placeholder="이름을 입력해주세요."
-                      />
-                    </td>
-                  </tr>
-                </tbody>
-                <tbody>
-                  <tr>
-                    <td>
-                      <div className="account-info-not-null">*</div>
-                      <div className="account-info-title ">영문이름</div>
-                      <input
-                        name="eng_name"
-                        type="text"
-                        onChange={onChange}
-                        defaultValue={accountInfo?.eng_name}
-                        placeholder="영문 이름을 입력해주세요."
-                      />
-                    </td>
-                  </tr>
-                </tbody>
-                <tbody>
-                  <tr>
-                    <td>
-                      <div className="account-info-not-null">*</div>
-                      <div className="account-info-title">이메일</div>
-                      <input
-                        name="email"
-                        type="text"
-                        onChange={onChange}
-                        defaultValue={accountInfo?.email}
-                        placeholder="포토폴리오 소식을 받을 이메일을 입력해주세요."
-                      />
-                    </td>
-                  </tr>
-                </tbody>
-                <tbody>
-                  <tr>
-                    <td>
-                      <div className="account-info-not-null">*</div>
-                      <div className="account-info-title">닉네임</div>
-                      <input
-                        name="nickname"
-                        type="text"
-                        onChange={onChange}
-                        defaultValue={accountInfo?.nickname}
-                        placeholder="닉네임을 입력해주세요."
-                      />
-                    </td>
-                  </tr>
-                </tbody>
-                <tbody>
-                  <tr>
-                    <td className="account-info-img-part-wrapper">
-                      <div className="account-info-title special">
-                        프로필 이미지
-                      </div>
-                      <div className="account-info-img-wrapper">
-                        {accountInfo.profile_image ? (
-                          <img
-                            name="profile_image"
-                            src={accountInfo.profile_image || ''}
-                            alt=""
-                          />
-                        ) : (
-                          <img
-                            src="https://cdn-icons-png.flaticon.com/512/847/847969.png"
-                            alt=""
-                          />
-                        )}
-                        <div className="account-info-img-part2-wrapper">
-                          <div className="account-info-img-explain">
-                            프로필 이미지를 등록해 주세요. <br /> 180 x 180 픽셀
-                            크기의 이미지를 권장합니다.
-                          </div>
-                          <form>
-                            <label
-                              className="dd"
-                              for="btn account-info-img-upload-btn"
-                            >
-                              이미지 업로드
-                            </label>
-                            <input
-                              type="file"
-                              className="btn account-info-img-upload-btn"
-                              style={{ display: 'none' }}
-                            />
-                          </form>
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              <hr />
-              <div className="account-info-contract">
-                이용자는 개인정보 수집 및 이용 동의를 거부할 권리가 있으나, 거부
-                시 채널 정보 등록이 불가능합니다. <br />
-                입력하신 정보(필수 - 이름, 영문이름, 프로필 이미지, 기본 이메일,
-                선택 - 국가, 외부공개 이메일)는 그라폴리오 서비스 제공을
-                목적으로 수집하며,
-                <b> 네이버 회원 탈퇴 혹은 직접 삭제 전까지 보관됩니다.</b>
-              </div>
-              <div className="account-info-btn-wrapper">
-                <button
-                  className="btn account-info-save-btn"
-                  onClick={saveAccountInfo}
-                >
-                  동의 및 저장
-                </button>
-                <button
-                  className="btn account-info-reset-btn"
-                  onClick={() => navigate('/works')}
-                >
-                  취소
-                </button>
-                <div>
-                  <button
-                    className="btn account-info-wdraw-btn"
-                    onClick={showModal}
-                  >
-                    채널삭제
-                  </button>
-                  {modalOpen && <DeleteModal setModalOpen={setModalOpen} />}
-                </div>
-              </div>
-            </form>
+        <div className="detail-content-wrapper">
+          <div className="detail-content-pictures">
+            <img src={feedImg.img_url} alt="" />
+          </div>
+          {/* 태그 컴포넌트 */}
+          <div className="detail-tag-wrapper">
+            {tags.map((tag, index) => {
+              return <Tag key={index} tag_name={tag.tag_name} />;
+            })}
+          </div>
+          <div className="detail-copy-right">
+            Copyright © {cardDetailContents.kor_name} All Rights Reserved.
           </div>
         </div>
+        {/* 공감 */}
+        <div className="detail-reaction-wrapper">
+          <div className="detail-reaction-inner-wrapper">
+            <div className="detail-reaction-icon-wrapper">
+              <button className="detail-icon">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/1062/1062573.png"
+                  alt=""
+                />
+              </button>
+            </div>
+            <div className="detail-reaction-icon-second-wrapper">
+              <div className="detail-reaction-icon-title-wrapper">
+                <div className="detail-icon-title">좋아요</div>
+              </div>
+              <div className="detail-reaction-icon-count-wrapper">
+                <div className="detail-icon-count">
+                  {/* {likeCnt} */}
+                  {/* {sympathys.sympathy_cnt} */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="detail-reply-out-wrapper">
+          <div className="detail-reply-wrapper">
+            <span className="detail-reply-icon">
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/6460/6460733.png"
+                alt=""
+              />
+            </span>
+          </div>
+        </div>
+        <details>
+          <summary />
+          <div className="detail-reply-input-out-wrapper">
+            <div className="detail-reply-input-wrapper">
+              <div className="detail-reply-input-inner-wrapper">
+                <div className="detail-reply-title">
+                  {localStorage.getItem('kor_name')}
+                </div>
+                <div className="detail-reply-text-area-wrapper">
+                  {localStorage.getItem('token') ? (
+                    <textarea
+                      type="text"
+                      placeholder="주제와 무관한 댓글, 악플은 삭제될 수 있습니다."
+                      ref={reply}
+                    />
+                  ) : (
+                    <textarea
+                      type="text"
+                      placeholder="댓글을 작성하려면 로그인 해주세요."
+                      onClick={movePage}
+                    />
+                  )}
+                </div>
+                <div className="detail-reply-text-limit">최대 1000자</div>
+              </div>
+              <div className="detail-reply-apload-btn">
+                <button onClick={saveReply}>등록</button>
+              </div>
+            </div>
+            <div className="detail-reply-list">
+              {/* 댓글 컴포넌트 */}
+              {replyArray.map(reply => {
+                return (
+                  <Reply
+                    key={reply.id}
+                    user_id={reply.user_id}
+                    kor_name={reply.kor_name}
+                    comment={reply.comment}
+                    created_at={reply.created_at}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </details>
       </div>
     </div>
   );
 };
 
-export default AccountInfo;
+export default CardDetailContents;
